46404fbae16818192e9a54cc1d1b4061698cb24d
keenwon <semanwmj@gmail.com>
2018-12-31 00:10:40 +0800

    build: rollup build

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import { terser } from 'rollup-plugin-terser'
+import { version } from './package.json'
+
+const banner = `/*!
+ * jq-paginator v${version}
+ * http://jqPaginator.keenwon.com
+ */
+`
+
+module.exports = [
+  {
+    input: './src/jq-paginator.js',
+    output: {
+      file: './dist/jq-paginator.js',
+      banner,
+      format: 'iife'
+    }
+  },
+  {
+    input: './src/jq-paginator.js',
+    plugins: [
+      terser({
+        sourcemap: true,
+        output: {
+          comments: /^!/
+        }
+      })
+    ],
+    output: {
+      file: './dist/jq-paginator.min.js',
+      banner,
+      format: 'iife',
+      sourcemap: true
+    }
+  }
+]
